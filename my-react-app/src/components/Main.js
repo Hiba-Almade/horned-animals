7db611e29ab0e1a1107cf61464e6e1571f1eaad7
Hiba-Almade <hibaalmade998@gmail.com>
2021-08-02 13:04:31 +0300

    fix lab1

--- my-react-app/src/components/Main.js
+++ my-react-app/src/components/Main.js
@@ -5,8 +5,15 @@ export class Main extends Component {
     render() {
         return (
             <div>
-               <HornedBeasts/> 
-               <HornedBeasts/>
+               <HornedBeasts imgurl ="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" 
+               title = "UniWhal" 
+               desc = "A unicorn and a narwhal nuzzling their horns"
+               /> 
+
+               <HornedBeasts imgurl = "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80" 
+               title = "Rhino Family"
+                desc = "Mother (or father) rhino with two babies"
+                /> 
             </div>
         )
     }
