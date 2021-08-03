37d407d89b1dcbb9d8987eec2406e7c681c552e6
Hiba-Almade <hibaalmade998@gmail.com>
2021-08-03 10:30:02 +0300

    fixlab1

--- my-react-app/src/components/Main.js
+++ my-react-app/src/components/Main.js
@@ -1,32 +1,32 @@
 import React, { Component } from 'react'
 import HornedBeasts from './HornedBeasts'
 import d from './data.json'
-import {Col} from 'react-bootstrap'
+
 
 export class Main extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            data:d
+        this.state = {
+            data: d
         }
         console.log(this.state.data);
     }
-    
+
     render() {
         return (
             <div>
                 {
-                this.state.data.map(element => {
-                     return (
-                     <Col xs={3} md={4}>
-                     <HornedBeasts imgurl ={element.image_url}
-                     title = {element.title}
-                     desc = {element.description}
-                     /> 
-                     </Col>
-                     )
-                })}
-               
+                    this.state.data.map(element => {
+                        return (
+
+                            <HornedBeasts imgurl={element.image_url}
+                                title={element.title}
+                                desc={element.description}
+                            />
+
+                        )
+                    })}
+
             </div>
         )
     }
